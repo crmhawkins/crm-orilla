d33c7b7bc14d04cf8e915fe27fa46c36c793beb9
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-04-09 11:12:24 +0200

    listo

--- resources/views/emails/reservationReminder.blade.php
+++ resources/views/emails/reservationReminder.blade.php
@@ -21,7 +21,7 @@
             max-width: 600px;
             margin: 20px auto;
             padding: 20px;
-            background-color: rgba(237, 225, 221, 0.9); /* Fondo semitransparente para mejorar la legibilidad */
+            background-color: #EDE1DD;
             border-radius: 8px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
         }

90d5dab2d6d3bd1bd40255dfd0ada011d59efd9c
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-07-01 09:32:02 +0200

    Correccion visual

--- resources/views/livewire/settings/settings-component.blade.php
+++ resources/views/livewire/settings/settings-component.blade.php
@@ -13,7 +13,7 @@
         </div> <!-- end row -->
     </div>
     <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-9 col-12 order-2 order-md-1">
             <div class="card mb-4 shadow-sm">
                 <div class="card-body">
                     <h4 class="card-title mb-4">Mesas para reservar</h4>
@@ -39,20 +39,28 @@
                         <div class="row g-3">
                                 <div class="col-md-6"><label for="inicio">Inicio</label></div>
                                 <div class="col-md-6"><label for="fin">Fin</label></div>
-                            @foreach ($dias as $index => $dia)
-                                <div class="col-md-12 d-flex align-items-center">
-                                    <input wire:model="dias.{{ $index }}.inicio" type="date" class="form-control" placeholder="Número de mesas">
-                                    <input wire:model="dias.{{ $index }}.fin" type="date" class="form-control mx-2" placeholder="Comensales por mesa">
-                                    <button wire:click="removeDia({{ $index }})" class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                                @foreach ($dias as $index => $dia)
+                                <div class="col-12">
+                                    <div class="d-flex flex-wrap align-items-center">
+                                        <div class="flex-fill p-1">
+                                            <input wire:model="dias.{{ $index }}.inicio" type="date" class="form-control" placeholder="Fecha de inicio">
+                                        </div>
+                                        <div class="flex-fill p-1">
+                                            <input wire:model="dias.{{ $index }}.fin" type="date" class="form-control" placeholder="Fecha de fin">
+                                        </div>
+                                        <div class="p-1">
+                                            <button wire:click="removeDia({{ $index }})" class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                                        </div>
+                                    </div>
                                 </div>
-                            @endforeach
+                                @endforeach
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card mb-4 shadow-sm position-fixed">
+        <div class="col-md-3 col-12 order-1 order-md-2">
+            <div class="card mb-4 shadow-sm">
                 <div class="card-body">
                     <h5 class="card-title">Opciones de guardado</h5>
                     <div class="d-grid gap-2">
